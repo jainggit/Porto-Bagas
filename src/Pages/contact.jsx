81bda94ac0a5278c6c5d34ff208c686e086b8982
jainggit <bagasarjuna89@gmail.com>
2025-07-30 10:50:22 +0700

    done-myporto

--- src/Pages/contact.jsx
+++ src/Pages/contact.jsx
@@ -53,7 +53,7 @@ const Contact = () => {
                         type="text"
                         name="name" 
                         required
-                        className="w-full p-2 border rounded bg-white"
+                        className="w-full p-2 border rounded bg-white text-black"
                         placeholder="Bagas"
                     />
                 </div>
@@ -63,7 +63,7 @@ const Contact = () => {
                         type="email"
                         name="email"
                         required
-                        className="w-full p-2 border rounded bg-white"
+                        className="w-full p-2 border rounded bg-white text-black"
                         placeholder="bagas@example.com"
                     />
                 </div>
@@ -72,7 +72,7 @@ const Contact = () => {
                     <textarea
                         name="message"
                         required
-                        className="w-full p-2 border rounded h-28 bg-white"
+                        className="w-full p-2 border rounded h-28 bg-white text-black"
                         placeholder="Tell me more about your idea or question"
                     />
                 </div>
